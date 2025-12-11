fc919285c6ae5317cf78044a6102e6fddf236115
salitsamyl <salitsameylani@gmail.com>
2025-12-11 21:20:48 +0700

    update

--- lib/helper/todos_helper.dart
+++ lib/helper/todos_helper.dart
@@ -8,7 +8,10 @@ class TodosHelper {
     var uri = Uri.parse("https://dummyjson.com/todos");
     var respon = await http.get(uri);
     if(respon.statusCode == 200){
-      List<dynamic> hasil = jsonDecode(respon)
+      List<dynamic> hasil = jsonDecode(respon.body);
+      return hasil.map((json)=>TodosModel.fromMap(json)).toList();
+    } else{
+      throw Exception("Koneksi Terganggu");
     }
   }
 }
--- lib/pages/todos.dart
+++ lib/pages/todos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salitsa_2410910040020_ujian_api/helper/todos_helper.dart';
 
 class Todos extends StatefulWidget {
   const Todos({super.key});
@@ -11,7 +12,17 @@ class _TodosState extends State<Todos> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: EdgeInsets.all(20)),
+      appBar: AppBar(
+        title: Text("Product Page"),),
+        body: FutureBuilder(
+          future: TodosHelper().getAllTodos(),
+          builder: (context,snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child: CircularProgressIndicator(),);
+            }
+            return Padding(padding: EdgeInsets.all(20));
+          }
+        ),
     );
   }
 }
