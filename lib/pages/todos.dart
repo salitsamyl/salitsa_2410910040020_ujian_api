2a87b1ba50ccb9cb1f01367de807b87e69c13679
salitsamyl <salitsameylani@gmail.com>
2025-12-11 21:34:46 +0700

    update

--- lib/pages/todos.dart
+++ lib/pages/todos.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:salitsa_2410910040020_ujian_api/helper/todos_helper.dart';
+import 'package:salitsa_2410910040020_ujian_api/models/todos_model.dart';
 
 class Todos extends StatefulWidget {
   const Todos({super.key});
@@ -13,15 +14,24 @@ class _TodosState extends State<Todos> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Product Page"),),
+        title: Text("Todos Page"),),
         body: FutureBuilder(
           future: TodosHelper().getAllTodos(),
           builder: (context,snapshot) {
             if(snapshot.connectionState == ConnectionState.waiting){
               return Center(child: CircularProgressIndicator(),);
             }
-            return Padding(padding: EdgeInsets.all(20));
-          }
+            if(snapshot.hasData){
+              List<TodosModel> todos = snapshot.data!;
+            }
+            return ListView.builder(
+              itemBuilder: (context, index){
+                return Padding(padding: EdgeInsets.all(20),
+                child: Text(todos[index]),
+                );
+              }
+            );
+          },
         ),
     );
   }
