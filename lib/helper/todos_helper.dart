9fd4de145a4e3c146d1d4cecedf143d8a6757fb1
salitsamyl <salitsameylani@gmail.com>
2025-12-11 21:13:14 +0700

    update

---
+++ lib/helper/todos_helper.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:salitsa_2410910040020_ujian_api/models/todos_model.dart';
+
+class TodosHelper {
+  Future<List<TodosModel>> getAllTodos() async {
+    var uri = Uri.parse("https://dummyjson.com/todos");
+    var respon = await http.get(uri);
+    if(respon.statusCode == 200){
+      List<dynamic> hasil = jsonDecode(respon)
+    }
+  }
+}
