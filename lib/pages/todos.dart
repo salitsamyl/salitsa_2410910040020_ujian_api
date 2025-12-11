08f369f6f27b57660b6e8a517b6c22a9b505134b
salitsamyl <salitsameylani@gmail.com>
2025-12-11 21:49:08 +0700

    update

--- lib/pages/todos.dart
+++ lib/pages/todos.dart
@@ -21,13 +21,17 @@ class _TodosState extends State<Todos> {
             if(snapshot.connectionState == ConnectionState.waiting){
               return Center(child: CircularProgressIndicator(),);
             }
-            if(snapshot.hasData){
-              List<TodosModel> todos = snapshot.data!;
+            if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text("Data tidak ditemukan"));
             }
+            List<TodosModel> todos = snapshot.data!;
             return ListView.builder(
+              itemCount: todos.length,
               itemBuilder: (context, index){
-                return Padding(padding: EdgeInsets.all(20),
-                child: Text(todos[index]),
+                return Padding(padding: EdgeInsets.only(right: 15, left: 15, bottom: 5, top: 10),
+                child: Container(
+                  decoration: ,
+                ),
                 );
               }
             );
