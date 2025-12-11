999b7c22f2f5b92c9786aee047f338f55c8dc0b4
salitsamyl <salitsameylani@gmail.com>
2025-12-11 19:29:15 +0700

    update

---
+++ lib/pages/registrasi.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Registrasi extends StatefulWidget {
+  const Registrasi({super.key});
+
+  @override
+  State<Registrasi> createState() => _RegistrasiState();
+}
+
+class _RegistrasiState extends State<Registrasi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
